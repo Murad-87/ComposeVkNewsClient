e2a949411879ac0f4e006c94f649fbbe97c2aab2
Murad <engno_95@mail.ru>
2024-01-19 16:47:23 +0300

    настройка главного экрана и карточки с группы

---
+++ app/src/main/java/com/muslim/simplevknewsclient/VkNewsMainScreen.kt
@@ -0,0 +1,96 @@
+package com.muslim.simplevknewsclient
+
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val action = snackbarHostState.showSnackbar(
+                                message = "This is snackbar",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+                            if (action == SnackbarResult.ActionPerformed) {
+                                fabIsVisible.value = false
+                            }
+                        }
+                    }
+                ) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
+        bottomBar = {
+            NavigationBar {
+
+                val selectedItemPosition = remember {
+                    mutableStateOf(0)
+                }
+
+                val items = listOf(
+                    NavigationItem.Home,
+                    NavigationItem.Favourite,
+                    NavigationItem.Profile
+                )
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemPosition.value == index,
+                        onClick = { selectedItemPosition.value = index },
+                        icon = { Icon(item.icon, contentDescription = null) },
+                        label = { Text(text = stringResource(id = item.titleResId)) },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                            unselectedIconColor = MaterialTheme.colorScheme.onSecondary,
+                            selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                            unselectedTextColor = MaterialTheme.colorScheme.onSecondary,
+                            indicatorColor = Color.White
+                        )
+                    )
+                }
+            }
+        }
+    ) {
+
+    }
+}
